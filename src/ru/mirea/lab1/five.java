a52f44f4bd4675e21c756361e3faadc7d1d570d4
Student Universiteta <artezem2004@gmail.com>
2023-09-22 07:30:42 +0300

    v1

---
+++ src/ru/mirea/lab1/five.java
@@ -0,0 +1,16 @@
+package ru.mirea.lab1;
+
+public class five {
+    // факториал
+    public static void fact(int num)
+    {
+        int res = 1;
+        for (int i = 1; i <= num; i++) {
+            res *= i;
+        }
+        System.out.println(res);
+    }
+    public static void main(String[] args) {
+        fact(5);
+    }
+}
