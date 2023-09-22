b7bf86de9a75a6e7a0f37accb7d9fe8a4684a839
Student Universiteta <artezem2004@gmail.com>
2023-09-22 07:27:11 +0300

    v1

---
+++ src/ru/mirea/lab1/three.java
@@ -0,0 +1,11 @@
+package ru.mirea.lab1;
+public class three
+{
+    public static void main(String[] args)
+    {
+        for (int i=0; i<args.length; i++)
+        {
+            System.out.println("args['' + i + ''] = \"" + args[i] + "\""); // вывод аргументов
+        }
+    }
+}
