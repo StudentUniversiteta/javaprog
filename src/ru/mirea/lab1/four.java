ca93f7f37cc9056b26aad89a3bbe276d6781ebc3
Student Universiteta <artezem2004@gmail.com>
2023-09-22 07:29:08 +0300

    v1

---
+++ src/ru/mirea/lab1/four.java
@@ -0,0 +1,13 @@
+package ru.mirea.lab1;
+
+public class four {
+
+    public static void main(String[] args)
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            System.out.print(i + " число = ");
+            System.out.println(1.0/i);
+        }
+    }
+}
