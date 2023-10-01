a37184642a7d779ec06ad05e0b2bdbb5ee5e9408
Student Universiteta <artezem2004@gmail.com>
2023-10-02 02:47:47 +0300

    v1

---
+++ src/ru/mirea/lab4/four/Main.java
@@ -0,0 +1,16 @@
+package ru.mirea.lab4.four;
+
+public class Main {
+    public static void main(String[] args) {
+        Processor processor = new Processor("Intel Core i7", 3.8);
+        Memory memory = new Memory("DDR4", 16);
+        Monitor monitor = new Monitor("1920x1080");
+
+        Computer computer = new Computer(Brand.APPLE, processor, memory, monitor);
+
+        System.out.println("Brand: " + computer.getBrand());
+        System.out.println("Processor: " + computer.getProcessor().getModel() + ", " + computer.getProcessor().getSpeed() + " GHz");
+        System.out.println("Memory: " + computer.getMemory().getType() + ", " + computer.getMemory().getSize() + " GB");
+        System.out.println("Monitor: " + computer.getMonitor().getResolution());
+    }
+}
---
+++ src/ru/mirea/lab4/three/Main.java
@@ -0,0 +1,47 @@
+package ru.mirea.lab4.three;
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Store store = new Store();
+        store.addUser(new User("a", "a"));
+        store.addProduct(new Product("Cell Phone", 15000.0, Category.ELECTRONICS));
+        store.addProduct(new Product("T-Shirt", 400.0, Category.CLOTHING));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя пользователя: ");
+        String username = scanner.nextLine();
+        System.out.println("Введите пароль: ");
+        String password = scanner.nextLine();
+
+        User user = store.authenticate(username, password);
+        if (user == null) {
+            System.out.println("Неверное имя пользователя или пароль");
+            return;
+        }
+
+        ShoppingCart cart = new ShoppingCart();
+        while (true) {
+            double sum = 0;
+            System.out.println("Список продуктов:");
+            List<Product> products = store.getProducts();
+            for (int i = 0; i < products.size(); i++) {
+                System.out.println((i + 1) + ". " + products.get(i).name + "\tКатегория " + products.get(i).category);
+            }
+            System.out.println("Введите номер продукта для добавления в корзину или 'checkout' для оплаты:");
+            String input = scanner.nextLine();
+            if (input.equals("checkout")) {
+                sum = cart.checkout();
+                System.out.print("Покупка совершена. Списано ");
+                System.out.print(sum);
+
+                break;
+            } else {
+                int productIndex = Integer.parseInt(input) - 1;
+                cart.addProduct(products.get(productIndex));
+                System.out.println("Продукт добавлен в корзину");
+            }
+        }
+    }
+}
