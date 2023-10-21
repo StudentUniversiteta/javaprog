535e68f9b9873cffbc80706b8a9583a717f5950e
StudentUniversiteta <artezem2004@gmail.com>
2023-10-21 14:27:41 +0300

    lab7

---
+++ src/ru/mirea/lab8/n7/factorisation.java
@@ -0,0 +1,32 @@
+package ru.mirea.lab8.n7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class factorisation {
+
+    public static List<Integer> getPrimeFactors(int n) {
+        List<Integer> factors = new ArrayList<>();
+        getPrimeFactorsRecursive(n, 2, factors);
+        return factors;
+    }
+
+    private static void getPrimeFactorsRecursive(int n, int divisor, List<Integer> factors) {
+        if (n < divisor) {
+            return;
+        }
+
+        if (n % divisor == 0) {
+            factors.add(divisor);
+            getPrimeFactorsRecursive(n / divisor, divisor, factors);
+        } else {
+            getPrimeFactorsRecursive(n, divisor + 1, factors);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 120;
+        List<Integer> primeFactors = getPrimeFactors(n);
+        System.out.println("Prime factors of " + n + ": " + primeFactors.toString());
+    }
+}
---
+++ src/ru/mirea/lab9/n1/l9n1Main.java
@@ -0,0 +1,5 @@
+package ru.mirea.lab9.n1;
+
+public class l9n1Main
+{
+}
