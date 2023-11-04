f31dd4eba3871075d5363add8b3946c261ba6a1c
Student Universiteta <artezem2004@gmail.com>
2023-11-04 16:01:22 +0300

    v1

---
+++ src/ru/mirea/lab15_GUI_Layout_3/n4/tester.java
@@ -0,0 +1,14 @@
+package ru.mirea.lab15_GUI_Layout_3.n4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class tester
+{
+    public static void main(String[] args)
+    {
+        Calculator_2 form4 = new Calculator_2();
+        form4.setVisible(true);
+    }
+}
