18e7032df1f763816544ad4d1c094fbb6b9826f6
Neksus42 <148706305+Neksus42@users.noreply.github.com>
2023-11-22 22:37:16 +0300

    Update Testing.java
    CHITAY PO RUSSKI

--- src/ru/mirea/lab20_Generics/Basics/Testing.java
+++ src/ru/mirea/lab20_Generics/Basics/Testing.java
@@ -6,6 +6,7 @@ public class Testing
 
     public static void main(String[] args)
     {
+        //HUI
         BasicGenerics<Integer> integerBasicGenerics = new BasicGenerics<>();
         integerBasicGenerics.add(42);
         int value = integerBasicGenerics.get();
